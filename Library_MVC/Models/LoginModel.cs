2c75a7fbe41837858d73f68bb9af0a9da450e912
yanerobot <screwyuguys@gmail.com>
2024-10-14 05:00:41 +0300

    Added email and password validation for Login

--- Library_MVC/Models/LoginModel.cs
+++ Library_MVC/Models/LoginModel.cs
@@ -1,15 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.RegularExpressions;
+using Library_MVC.Models.Attributes;
 
 namespace Library_MVC.Models
 {
 	public class LoginModel
 	{
+		[EmailAddress(ErrorMessage = "Введите корректный Email")]
 		[Required(ErrorMessage = "Не указан Email")]
-		[DataType(DataType.EmailAddress, ErrorMessage = "Пожалуйста, введите корректный адрес электронной почты")]
 		public string? Email { get; set; }
 
+		[PasswordValidation(ErrorMessage = "Пароль не соответствует требованиям.")]
 		[Required(ErrorMessage = "Не указан пароль")]
-		[DataType(DataType.Password)]
 		public string? Password { get; set; }
 	}
 }
